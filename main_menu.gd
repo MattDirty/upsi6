6341a91ecef907ce9bfe17b3e2fc1ce70be8e680
histidiine <aurelie.m.stephan@gmail.com>
2026-03-21 09:42:11 +0100

    sfx + put all powerups in one scene

--- main_menu.gd
+++ main_menu.gd
@@ -1,13 +1,30 @@
 extends Node2D
 
+func _on_start_mouse_entered() -> void:
+	%Hover.play()
+
+
+func _on_options_mouse_entered() -> void:
+	%Hover.play()
+
+
+func _on_quit_mouse_entered() -> void:
+	%Hover.play()
+
 
 func _on_start_pressed() -> void:
+	%Click1.play()
+	await get_tree().create_timer(.25).timeout
 	get_tree().change_scene_to_file("res://Playground.tscn")
 
 
 func _on_options_pressed() -> void:
+	%Click2.play()
+	await get_tree().create_timer(.25).timeout
 	get_tree().change_scene_to_file("")
 
 
 func _on_quit_pressed() -> void:
+	%Click3.play()
+	await get_tree().create_timer(.25).timeout
 	get_tree().quit()

d237c1399c0b324af2e20f9956b1a7c9bf8a30fd
histidiine <aurelie.m.stephan@gmail.com>
2026-03-24 10:40:02 +0100

    remplacer liste avec une liste de mots "safe" comme mes neveux veulent essayer le jeu ahaha

--- Systems/Words/Words.gd
+++ Systems/Words/Words.gd
@@ -1,12 +1,14 @@
 extends Node
 
-var text_file_path = "res://Assets/wordlist.10000.txt"
+#var text_file_path = "res://Assets/wordlist.10000.txt"
+var text_file_path = "res://Assets/safe-words.txt"
 
 var words: Array = []
 
 func _ready():
 	var text_content = get_text_file_content(text_file_path)
 	words = text_content.split("\n")
+	print(words)
 
 
 func get_text_file_content(filePath):
@@ -16,4 +18,4 @@ func get_text_file_content(filePath):
 
 
 func get_random_word():
-	return words[randi() % words.size()]
+	return words[randi() % words.size()].to_lower()
